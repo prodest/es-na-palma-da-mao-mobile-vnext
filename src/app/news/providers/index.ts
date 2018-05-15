6383f067e63b8a20b2f24a3a6c37caf5f26b3f47
vizeke <vizeke@gmail.com>
2018-05-15 19:27:21 -0300

    feat(noticias): Implementa serviço de API

---
+++ src/app/news/providers/index.ts
@@ -0,0 +1,5 @@
+import { NewsApiService } from './news-api.service';
+
+export { NewsApiService };
+
+export const NewsProviders = [NewsApiService];
---
+++ src/app/news/providers/news-api.service.ts
@@ -0,0 +1,80 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Environment, EnvVariables } from '@espm/core';
+import { Observable } from 'rxjs/Observable';
+import { share } from 'rxjs/operators';
+import { ANONYMOUS_HEADER } from './../../../libs/core/auth';
+
+import { Filter, News, NewsDetails, Pagination } from '../model';
+
+@Injectable()
+export class NewsApiService {
+  public static $inject: string[] = ['$http', 'settings'];
+
+  /**
+   * Creates an instance of NewsApiService.
+   *
+   * @param {IHttpService} $http
+   * @param {ISettings} settings
+   */
+  constructor(private http: HttpClient, @Inject(EnvVariables) private env: Environment) {}
+
+  /**
+   *
+   * @returns {*}
+   */
+  public getNewsById(id: string): Observable<NewsDetails> {
+    return this.http
+      .get<NewsDetails>(`${this.env.api.news}/${id}`, { headers: new HttpHeaders({ [ANONYMOUS_HEADER]: 'true' }) })
+      .pipe(share());
+  }
+
+  /**
+   *
+   * @returns {*}
+   */
+  public getHighlightNews(): Observable<News[]> {
+    return this.http
+      .get<News[]>(`${this.env.api.news}/highlights`, { headers: new HttpHeaders({ [ANONYMOUS_HEADER]: 'true' }) })
+      .pipe(share());
+  }
+
+  /**
+   *
+   * @param calendars
+   * @param options
+   * @returns {Array}
+   */
+  public getNews(filter: Filter, pagination: Pagination): Observable<News[]> {
+    let defaults = {
+      origins: [],
+      query: '',
+      pageNumber: this.env.pagination.pageNumber,
+      pageSize: this.env.pagination.pageSize
+    };
+
+    let params = Object.assign({}, defaults, filter, pagination);
+
+    return this.http.get<News[]>(this.env.api.news, {
+      params: new HttpParams()
+        .set('pageNumber', params.pageNumber.toString())
+        .set('pageSize', params.pageSize.toString())
+        .set('origins', JSON.stringify(params.origins))
+        .set('query', params.query)
+        .set('dateMin', params.dateMin)
+        .set('dateMax', params.dateMax),
+      headers: new HttpHeaders({ [ANONYMOUS_HEADER]: 'true' })
+    });
+  }
+
+  /**
+   *
+   * @returns {*}
+   */
+  public getAvailableOrigins(): Promise<string[]> {
+    return this.http
+      .get<string[]>(`${this.env.api.news}/origins`, { headers: new HttpHeaders({ [ANONYMOUS_HEADER]: 'true' }) })
+      .pipe(share())
+      .toPromise();
+  }
+}
