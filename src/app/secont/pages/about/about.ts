7ee67bc43645aaa823cb40b825a925d2b1c9d9d6
hoisel <danielhoiselferraz@gmail.com>
2018-06-07 17:46:23 -0300

    feat(transparencia): adiciona página "Sobre"

---
+++ src/app/secont/pages/about/about.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+@IonicPage({
+  segment: 'secont/sobre',
+  name: 'TransparenciaAboutPage'
+})
+@Component({
+  selector: 'page-transparencia-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {}
