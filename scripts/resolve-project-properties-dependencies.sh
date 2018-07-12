a18167dedce53f78a4eb249802918c5b0d70f974
hoisel <danielhoiselferraz@gmail.com>
2018-07-12 19:17:50 -0300

    fix(scritps): bump errado  nas versões das bibliotecas do android

--- scripts/resolve-project-properties-dependencies.sh
+++ scripts/resolve-project-properties-dependencies.sh
@@ -1,5 +1,5 @@
-sed -i 's/\(com.google.android.gms:[a-z-]*:\).*/\111.0.4/' ./platforms/android/project.properties
+sed -i 's/\(com.google.android.gms:.*:\).*/\111.0.4/' ./platforms/android/project.properties
 
-sed -i 's/\(com.android.support:[a-z-]*:\).*/\125.3.1/' ./platforms/android/project.properties
+sed -i 's/\(com.android.support:.*:\).*/\125.3.1/' ./platforms/android/project.properties
 
-sed -i 's/\(com.facebook.android:[a-z-]*:\).*/\14.25.0/' ./platforms/android/project.properties
+sed -i 's/\(com.facebook.android:.*:\).*/\14.25.0/' ./platforms/android/project.properties
