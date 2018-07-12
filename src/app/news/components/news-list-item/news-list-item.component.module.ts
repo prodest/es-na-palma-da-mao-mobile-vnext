76c2f9c90b4bb2977bb2d62eb37ae758f57f26f3
hoisel <danielhoiselferraz@gmail.com>
2018-07-12 16:39:03 -0300

    feat(noticias): adiciona loading nas imagens da lista de notícias

--- src/app/news/components/news-list-item/news-list-item.component.module.ts
+++ src/app/news/components/news-list-item/news-list-item.component.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ImageLoaderModule } from '@espm/shared';
 import { FromNowPipeModule } from '@espm/shared/pipes';
 import { IonicModule } from 'ionic-angular';
 
@@ -7,7 +8,7 @@ import { NewsListItemComponent } from './news-list-item.component';
 
 @NgModule({
   declarations: [NewsListItemComponent],
-  imports: [CommonModule, FromNowPipeModule, IonicModule],
+  imports: [CommonModule, ImageLoaderModule, FromNowPipeModule, IonicModule],
   exports: [NewsListItemComponent]
 })
 export class NewsListItemComponentModule {}
