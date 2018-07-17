152b218bee7f70a34bd6a3d19fb57298ee26676b
hoisel <danielhoiselferraz@gmail.com>
2018-07-17 15:26:02 -0300

    fix(auth): path errado para avatar de usuário default

--- src/libs/core/auth/models/user.ts
+++ src/libs/core/auth/models/user.ts
@@ -1,24 +1,6 @@
 import { AcessoCidadaoClaims } from './claims/acessoCidadaoClaims';
 
-// import { ID } from '@datorama/akita';
-
-export const defaultAvatarSrc = './assets/imgs/user.png';
-
-// export interface User extends AcessoCidadaoClaims {
-//   avatarUrl: string;
-// }
-
-// export type Credentials = {
-//   user: string;
-//   password: string;
-// };
-
-// export function createUser(claims: AcessoCidadaoClaims): Partial<User> {
-//   return {
-//     ...claims,
-//     avatarUrl: defaultAvatarSrc
-//   };
-// }
+export const defaultAvatarSrc = 'assets/imgs/user.png';
 
 export function createAnonymousUser(): Partial<AcessoCidadaoClaims> {
   return {
