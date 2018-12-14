d1046dc24edc36535cd27c06de9aab3625b0ce8b
Gary M. Silva <garymsilva@outlook.com>
2018-12-14 19:20:40 -0200

    feat(realtime): atualizando promises e TODOs

--- src/app/ceturb/transcol-online/pages/transcol-online-real-time/transcol-online-real-time.ts
+++ src/app/ceturb/transcol-online/pages/transcol-online-real-time/transcol-online-real-time.ts
@@ -17,16 +17,22 @@ import { TranscolOnlineRealTimeService } from './../../providers';
   templateUrl: 'transcol-online-real-time.html',
 })
 export class TranscolOnlineRealTimePage {
-  veiculos: [{
+  veiculos: Promise<[{
     linha: number,
     destino: string,
     distancia: number
-  }];
+  }]>;
 
   coordenadas: {
     lat: number,
     lon: number
   };
+
+  pontoProximo: {
+    id: number,
+    lat: number,
+    lon: number
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public transcolOnlineRealTimeService: TranscolOnlineRealTimeService, private geolocation: Geolocation) {
   }
@@ -34,14 +40,14 @@ export class TranscolOnlineRealTimePage {
   async ionViewWillLoad() {
     /* TO DO: Colocar loading enquanto a lista de veículos não fica pronta */
     await this.getCoordenadas();
-    this.veiculos = this.nearVehicles();
+    this.veiculos = await this.nearVehicles();
   }
 
   async ionViewDidLoad() {
     
   }
   
-  async getCoordenadas() {
+  async getCoordenadas(): Promise<void> {
     let resp: Geoposition;
 
     try {
@@ -53,9 +59,46 @@ export class TranscolOnlineRealTimePage {
     this.coordenadas = {lat: resp.coords.latitude, lon: resp.coords.longitude};
   }
 
-  nearVehicles(): any {
-    let veiculos = this.transcolOnlineRealTimeService.getNextVehicles(this.coordenadas);
+  findNearestStop() {
+
+  }
+
+  async nearVehicles(): Promise<any> {
+    let resp = await this.transcolOnlineRealTimeService.getNextVehicles(1);
+    /* mock */
+    let veiculos = [
+      {
+        linha: 516,
+        destino: "Terminal do Ibes",
+        distancia: 3,
+        numero: 23012
+      },
+      {
+        linha: 501,
+        destino: "Terminal de Vila Velha",
+        distancia: 3.2,
+        numero: 23013
+      },
+      {
+        linha: 516,
+        destino: "Terminal de Jacaraípe",
+        distancia: 3.3,
+        numero: 23014
+      }
+    ];
+
+    /*
+      TO DO:
+        - criar um Model para os veículos, com linha e itinerario
+        - instanciar os veículos e colocar no array
+      */
+    
     return veiculos;
+  }
+
+  /* TO DO: Calcular distância entre o aparelho e o ônibus */
+  private calcDistance() {
+
   }
 
 }
