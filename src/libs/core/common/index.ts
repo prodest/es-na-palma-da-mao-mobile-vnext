03dacf10d5de80aa5de4bf33cc41d2b16161695c
hoisel <danielhoiselferraz@gmail.com>
2018-12-21 19:33:24 -0200

    feat(core): adiciona toast service

--- src/libs/core/common/index.ts
+++ src/libs/core/common/index.ts
@@ -1,6 +1,7 @@
 import { ErrorHandlerInterceptorProvider } from './errors-handler.interceptor';
 import { LoadingService } from './loading.service';
+import { ToastService } from './toast.service';
 
-export { ErrorHandlerInterceptorProvider, LoadingService };
+export { ErrorHandlerInterceptorProvider, LoadingService, ToastService };
 
-export const Common = [ErrorHandlerInterceptorProvider, LoadingService];
+export const Common = [ErrorHandlerInterceptorProvider, LoadingService, ToastService];
