7e27cdc67cd068e7774891997b1d686ddc53052c
Gary M. Silva <garymsilva@outlook.com>
2019-01-30 14:38:53 -0200

    feat(vehiclesQuery): configurando SortBy distancia

--- src/app/ceturb/transcol-online/state/realtime-vehicle/vehicle.query.ts
+++ src/app/ceturb/transcol-online/state/realtime-vehicle/vehicle.query.ts
@@ -1,9 +1,12 @@
-import { QueryEntity } from '@datorama/akita';
+import { QueryEntity, QueryConfigOptions, QueryConfig, Order } from '@datorama/akita';
 import { VehiclesState, VehiclesStore } from './vehicle.store';
 import { Vehicle } from './vehicle.model';
 import { Injectable } from '@angular/core';
 
-
+@QueryConfig({
+  sortBy: 'distancia',
+  sortByOrder: Order.ASC
+} as QueryConfigOptions)
 @Injectable()
 export class VehiclesQuery extends QueryEntity<VehiclesState, Vehicle> {
   constructor(protected store: VehiclesStore) {
