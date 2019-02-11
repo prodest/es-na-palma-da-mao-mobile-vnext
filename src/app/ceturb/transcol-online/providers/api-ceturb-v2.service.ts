8a267b323dce918a0cab414b819d0b64cbd9facd
Gary M. Silva <garymsilva@outlook.com>
2019-02-11 20:02:20 -0200

    feat(apiceturbv2): Add métodos pra obter previsões com e sem intervalo

--- src/app/ceturb/transcol-online/providers/api-ceturb-v2.service.ts
+++ src/app/ceturb/transcol-online/providers/api-ceturb-v2.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, Inject } from '@angular/core';
-import { share } from 'rxjs/operators'
+import { share, map } from 'rxjs/operators';
 import { EnvVariables, Environment } from '@espm/core';
 
 @Injectable()
@@ -15,5 +15,33 @@ export class ApiCeturbV2Service {
    */
   allStops() {
     return this.http.get(`${this.env.api.ceturbv2}/pontos`).pipe(share());
+  }
+
+  /**
+   * Retorna as previsões de ônibus para um ponto.
+   * @param stopId - ID do ponto que se quer saber as previsões.
+   */
+  previsionsByStop(stopId: number) {
+    return this.http.get(`${this.env.api.ceturbv2}/estimativas/ponto/${stopId}/origem`)
+    .pipe(
+      map(previsions => previsions['estimativas'])
+    );
+  }
+
+  /**
+   * Retorna as previsões de ônibus para um ponto com um intervalo limite.
+   * @param stopId - ID do ponto que se quer saber as previsões.
+   * @param intervalInMinutes - Tempo em minutos que se quer saber as previsões.
+   */
+  previsionsByStopOnInterval(stopId: number, intervalInMinutes: number) {
+    const limitInterval = Date.now() + (intervalInMinutes * 60000);
+    return this.previsionsByStop(stopId)
+    .pipe(
+      map(
+        (previsions: Array<{}>) => previsions
+                            .filter((prevision) => prevision['horarioNaOrigem'] < limitInterval)
+                            .sort((a, b) => a['horarioNaOrigem'] - b['horarioNaOrigem'])
+      )
+    );
   }
 }
