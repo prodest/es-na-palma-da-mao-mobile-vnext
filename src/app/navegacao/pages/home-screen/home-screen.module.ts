f2dc1d8ec45ea1441149d1b3401995a94675f481
Gary M. Silva <garymsilva@outlook.com>
2019-06-08 22:55:47 -0300

    refactor(home-screen): Aplicando novo componente large-button

--- src/app/navegacao/pages/home-screen/home-screen.module.ts
+++ src/app/navegacao/pages/home-screen/home-screen.module.ts
@@ -3,12 +3,14 @@ import { IonicPageModule } from 'ionic-angular';
 import { HomeScreenPage } from './home-screen';
 
 import { LargeHeaderComponentModule } from './../../components/large-header';
+import { LargeButtonComponentModule } from '@espm/shared/components/large-button';
 
 @NgModule({
   declarations: [HomeScreenPage],
   imports: [
     IonicPageModule.forChild(HomeScreenPage),
-    LargeHeaderComponentModule
+    LargeHeaderComponentModule,
+    LargeButtonComponentModule
   ]
 })
 export class HomeScreenPageModule {}
