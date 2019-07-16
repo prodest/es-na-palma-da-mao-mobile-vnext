c96904a9b631c3c35db6d401fcd063c44837c401
Marcos Carneiro <cardepaula@gmail.com>
2019-07-16 19:03:04 -0300

    feat(component): Pagina para adicionar destinatarios do encaminhamento

--- src/app/edocs/pages/documents-to-send-add-addressees/documents-to-send-add-addressees.ts
+++ src/app/edocs/pages/documents-to-send-add-addressees/documents-to-send-add-addressees.ts
@@ -10,12 +10,44 @@ import { IonicPage, NavController } from 'ionic-angular';
   changeDetection: ChangeDetectionStrategy.Default
 })
 export class DocumentsToSendAddAddresseesPage implements OnInit {
-
   // addressees: string = ''
 
-  constructor(public navCtrl: NavController) { }
+  addresseesTypeFilter = [
+    {
+      id: 0,
+      type: 'Somente órgão',
+      notice:
+        'Ao enviar para uma organização, apenas o responsável pela organização (e pessoas explicitamente autorizadas por ele no Acesso Cidadão) terão acesso ao trâmite.'
+    },
+    {
+      id: 1,
+      type: 'Setor',
+      notice:
+        'Ao enviar para um setor, apenas o responsável pelo setor (e pessoas explicitamente autorizadas por ele no Acesso Cidadão) terão acesso ao trâmite.'
+    },
+    {
+      id: 2,
+      type: 'Grupos de Trabalho',
+      notice: 'Ao enviar para um grupo, TODOS os membros do grupo terão acesso ao trâmite.'
+    }
+  ];
+  selAddresseesTypeFilter: {id: number; type: string; notice: string} = this.addresseesTypeFilter[0];
+  
+  govAgency: string = '';
+  govSector: string = '';
+  govGroups: string = '';
 
-  ngOnInit(): void { }
+  constructor(public navCtrl: NavController) {}
 
-  refresh(): void { }
+  searchInput(event) {
+    console.log(event);
+  }
+
+  ngOnInit(): void {}
+
+  refresh(): void {}
+
+  isValidNumber(value: any) {
+    return typeof value === 'number' && !isNaN(value);
+  }
 }
