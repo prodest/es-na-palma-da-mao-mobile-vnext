2b12922d50516177adcb5f2548627368cb7cf691
David Vilaça <vilacapdavid@gmail.com>
2019-07-23 10:10:04 -0300

    feat(edocs): add wizard step abstract class

---
+++ src/app/edocs/interfaces/wizard-steps.ts
@@ -0,0 +1,12 @@
+/**
+ * @description wizard step output interface
+ * @author David Vilaça
+ * @date 2019-07-22
+ * @export
+ * @interface IBaseStepOutput
+ */
+export interface IBaseStepOutput {
+  titleForward: string;
+  sender: string;
+  role: string;
+}
