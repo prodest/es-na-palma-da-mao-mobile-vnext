5fabcdd905d7e41e3754cb25b3792a907bd8db90
Marcos Carneiro <cardepaula@gmail.com>
2019-07-31 14:47:21 -0300

    refactor: Change environments

--- src/libs/core/environment/environment.dev.ts
+++ src/libs/core/environment/environment.dev.ts
@@ -22,8 +22,8 @@ export const dev: Environment = {
     push: process.env.API_PUSH_URL || 'https://api.es.gov.br/developers/push/api/v1/',
     espm: process.env.API_ESPM_URL || 'https://api.es.gov.br/developers/espm',
     transparency: process.env.API_TRANSPARENCY_URL || 'https://api.es.gov.br/developers/transparency',
-    acessocidadaoApi: process.env.API_ACESSO_CIDADAO_URL || 'https://developers.es.gov.br/acessocidadao.webapi/api',
-    acessocidadao: process.env.ACESSO_CIDADAO_URL || 'https://developers.es.gov.br/acessocidadao',
+    acessocidadaoApi: process.env.API_ACESSO_CIDADAO_URL || 'https://sistemas.es.gov.br/prodest/acessocidadao.webapi/api',
+    acessocidadao: process.env.ACESSO_CIDADAO_URL || 'https://sistemas.es.gov.br/prodest/acessocidadao',
     feedback: process.env.API_FEEDBACK_URL || 'https://developers.es.gov.br/demandas',
     edocs: process.env.API_EDOCS_URL || 'http://api.e-docs.des.es.gov.br',
     empregabilidade: process.env.API_EMPREGABILIDADE || 'https://api.es.gov.br/dev/selecaodt/concursos',
@@ -44,8 +44,8 @@ export const dev: Environment = {
   },
   locale: 'pt-br',
   identityServer: {
-    url: process.env.IDENTITY_SERVER_URL || 'https://developers.es.gov.br/acessocidadao/is',
-    defaultScopes: 'openid offline_access ApiAcessoCidadao cpf nome email documentos api-acessocidadao-base api-acessocidadao-servidores',
+    url: process.env.IDENTITY_SERVER_URL || 'https://acessocidadao.es.gov.br/is',
+    defaultScopes: 'openid offline_access ApiAcessoCidadao cpf nome email documentos api-acessocidadao-base api-acessocidadao-servidores api-sigades-documento',
     publicKey:
       '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArYaYlPnnrxBVwC4o0ykG\nVg8gjH/TerrrXS3GmsZeON6SCNuOBzUj+7RiEF64lE//gLY01nTJZtnUIPvmKJW/\n1+eWxGNW1Mh1JpT/f3A6Q5rp2WNKSBwvEFPE58lkD63Tewsn3+0dw+aFKaSW+l3A\nZ7WS4AxXxBLIRr2zpTL3DOCbeT/m2yEQ8Do662/d+ty7F08FJVaaz2PxmnLEeSQX\n6RTRPeFRPlGVj91H4h85Ln+0Oc0U/oiqa+AKwobWXLOqDKhn8HYZuoya368TqZ9X\n26QEp1g7psaT8kiNRFAt0Yb4WbgFSWf2r92HDS8dj25TNTeeLkvZ48KylTKU23DT\nqQIDAQAB\n-----END PUBLIC KEY-----',
     clients: {
