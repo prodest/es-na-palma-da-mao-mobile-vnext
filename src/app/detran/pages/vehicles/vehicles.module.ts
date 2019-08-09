98a0e6b4581ea48751f8c41f12c5801e3b3db885
Gary M. Silva <garymsilva@outlook.com>
2019-08-09 19:10:52 -0300

    feat(meus-veiculos): aplicando componentes de header e footer

--- src/app/detran/pages/vehicles/vehicles.module.ts
+++ src/app/detran/pages/vehicles/vehicles.module.ts
@@ -3,9 +3,15 @@ import { IonicPageModule } from 'ionic-angular';
 
 import { VehicleComponentModule } from '../../components';
 import { VehiclesPage } from './vehicles';
+import { NavHeaderComponentModule, MainFooterBarComponentModule } from '@espm/shared/components';
 
 @NgModule({
   declarations: [VehiclesPage],
-  imports: [VehicleComponentModule, IonicPageModule.forChild(VehiclesPage)]
+  imports: [
+    VehicleComponentModule,
+    IonicPageModule.forChild(VehiclesPage),
+    NavHeaderComponentModule,
+    MainFooterBarComponentModule
+  ]
 })
 export class VehiclesPageModule {}
