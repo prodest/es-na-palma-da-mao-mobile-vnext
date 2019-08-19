6826dbce932b0fd65c46bce67898b2908dfd163b
Ivonildo <santosivonildobispo@gmail.com>
2019-08-19 16:33:59 -0300

    feat(mobile): modularização default

--- src/app/calendar/pages/calendar-filter/calendar-filter.module.ts
+++ src/app/calendar/pages/calendar-filter/calendar-filter.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CalendarFilterPage } from './calendar-filter';
 
+
+import { MainFooterBarComponentModule, NavTitleComponentModule } from '@espm/shared/components';
+
 @NgModule({
   declarations: [CalendarFilterPage],
-  imports: [IonicPageModule.forChild(CalendarFilterPage)]
+  imports: [IonicPageModule.forChild(CalendarFilterPage),
+    NavTitleComponentModule,
+    MainFooterBarComponentModule ]
 })
 export class CalendarFilterPageModule {}
