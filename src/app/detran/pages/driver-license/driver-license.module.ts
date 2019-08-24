db26287af13bc6b52fc35fc2b16df2549a9f81ff
Gary M. Silva <garymsilva@outlook.com>
2019-08-24 19:35:56 -0300

    refactor(driver-license): Aplicando module-page ao DriverLicense

--- src/app/detran/pages/driver-license/driver-license.module.ts
+++ src/app/detran/pages/driver-license/driver-license.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 
 import { DriverLicensePage } from './driver-license';
+import { ModulePageComponentModule } from '@espm/shared/components';
 
 @NgModule({
   declarations: [DriverLicensePage],
-  imports: [IonicPageModule.forChild(DriverLicensePage)]
+  imports: [IonicPageModule.forChild(DriverLicensePage), ModulePageComponentModule]
 })
 export class DriverLicensePageModule {}
