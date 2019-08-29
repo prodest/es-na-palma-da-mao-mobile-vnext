02bd9fec5b455cbb1575c14619da4362866f2028
Gary M. Silva <garymsilva@outlook.com>
2019-08-29 18:03:41 -0300

    refactor(all): Refatorando guideline

--- src/app/oportunidades/concurso/pages/apresentacao/apresentacao.module.ts
+++ src/app/oportunidades/concurso/pages/apresentacao/apresentacao.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ApresentacaoPage } from './apresentacao';
+import { ModuleIndexComponentModule } from '@espm/shared/components';
 
 @NgModule({
   declarations: [ApresentacaoPage],
-  imports: [IonicPageModule.forChild(ApresentacaoPage)]
+  imports: [
+    ModuleIndexComponentModule,
+    IonicPageModule.forChild(ApresentacaoPage)
+  ]
 })
 export class ApresentacaoPageModule {}
