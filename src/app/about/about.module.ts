e73b7f8f4d77dab01818886429af7f56f2b91fd6
Gary M. Silva <garymsilva@outlook.com>
2019-09-05 17:12:16 -0300

    refactor(about): Aplicando novo layout no Sobre

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AboutPage } from './about';
+import { ModulePageComponentModule } from '@espm/shared/components';
 
 @NgModule({
   declarations: [AboutPage],
-  imports: [IonicPageModule.forChild(AboutPage)]
+  imports: [
+    ModulePageComponentModule,
+    IonicPageModule.forChild(AboutPage)
+  ]
 })
 export class AboutPageModule {}
