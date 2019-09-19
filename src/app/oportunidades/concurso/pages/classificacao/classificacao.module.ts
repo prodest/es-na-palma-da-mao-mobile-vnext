6b6497becddaacf8d1d6254f4c1ef2169cea08e6
Gary M. Silva <garymsilva@outlook.com>
2019-09-19 12:03:05 -0300

    refactor(dt-classificacao): Importa module-page e adapta ao guideline

--- src/app/oportunidades/concurso/pages/classificacao/classificacao.module.ts
+++ src/app/oportunidades/concurso/pages/classificacao/classificacao.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ClassificacaoPage } from './classificacao';
+import { ModulePageComponentModule } from '@espm/shared/components';
 
 @NgModule({
   declarations: [
     ClassificacaoPage,
   ],
   imports: [
+    ModulePageComponentModule,
     IonicPageModule.forChild(ClassificacaoPage),
   ],
 })
