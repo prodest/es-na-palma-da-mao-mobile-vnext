a632664837b6874f9b0a33370ee195c1838f90cc
Marcos A C de Paula <cardepaula@gmail.com>
2019-09-25 14:46:35 -0300

    feat(documents-to-send): Tirar foto e encaminha-la pelo app

--- src/app/edocs/components/documents-to-send-doc-form/documents-to-send-doc-form.component.ts
+++ src/app/edocs/components/documents-to-send-doc-form/documents-to-send-doc-form.component.ts
@@ -19,6 +19,7 @@ import { Subscription } from 'rxjs/Subscription';
 import { FormBase, LoadingService } from '@espm/core';
 import { DocumentoNatureza, DocumentFile } from '../../state';
 import { File, IFile } from '@ionic-native/file';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'edocs-documents-to-send-doc-form',
@@ -80,7 +81,8 @@ export class DocumentsToSendBasicFormComponent extends FormBase implements OnIni
     private alertCtrl: AlertController,
     private platform: Platform,
     private f: File,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private camera: Camera
   ) {
     super(formBuilder);
   }
@@ -183,5 +185,48 @@ export class DocumentsToSendBasicFormComponent extends FormBase implements OnIni
       delete model.documentAssignType;
     }
     return model;
+  }
+
+  async takePhoto() {
+
+    try {
+      const loading = this.loadingService.show('Aguarde');
+      const options: CameraOptions = {
+        quality: 100,
+        destinationType: this.camera.DestinationType.FILE_URI,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE,
+        correctOrientation: true,
+        saveToPhotoAlbum: true,
+        allowEdit: true,
+      }
+    
+      
+      const imageUri = await this.camera.getPicture(options);
+      console.log(imageUri);
+
+      const response: any = await this.f.resolveLocalFilesystemUrl(imageUri);
+      response.file((file: IFile) => {
+        const docFile: DocumentFile = {
+          url: imageUri,
+          name: file.name,
+          type: file.type
+        };
+        this.form.get('file').setValue(docFile);
+        this.onFileSelect.next(docFile);
+        console.log(docFile);
+        loading.dismiss();
+      })
+    
+      this.cdr.detectChanges();
+    } catch (e) {
+      console.error('Error catch file chooser', e);
+      const alert = this.alertCtrl.create({
+        title: 'Erro inesperado',
+        message: 'Não foi possível tirar a foto. Tente novamente mais tarde.',
+        buttons: ['Ok']
+      });
+      alert.present();
+    }
   }
 }
