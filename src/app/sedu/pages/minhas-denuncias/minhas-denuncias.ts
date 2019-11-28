2884ba07a577bbe7f91f74fabfd2dbcc5cd16015
Gary M. Silva <garymsilva@outlook.com>
2019-11-28 19:50:52 -0300

    feat(sedu-minhas-denuncias): Implementa a tela minhas denúncias

--- src/app/sedu/pages/minhas-denuncias/minhas-denuncias.ts
+++ src/app/sedu/pages/minhas-denuncias/minhas-denuncias.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
+import { SeduDenunciasApiService } from '../../providers';
+import { Subject } from 'rxjs/Subject';
+import { AuthQuery } from '@espm/core';
 
 @IonicPage()
 @Component({
@@ -7,4 +10,25 @@ import { IonicPage } from 'ionic-angular';
   templateUrl: 'minhas-denuncias.html',
 })
 export class MinhasDenunciasPage {
+
+  denuncias$: Subject<any>;
+
+  constructor(
+    private api: SeduDenunciasApiService,
+    public auth: AuthQuery
+  ) {
+    this.denuncias$ = new Subject();
+  }
+
+  ionViewWillLoad() {
+    this.api.getUserDemands(this.auth.state.claims.nome)
+    .subscribe((denuncias) => {
+      console.log(denuncias);
+      this.denuncias$.next(denuncias);
+    });
+  }
+
+  date(date: string) {
+    return (new Date(date)).toLocaleString();
+  }
 }
