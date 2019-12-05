073912e13c61752c43a373a4e3342d6be69862f7
David Vilaça <vilacapdavid@gmail.com>
2019-12-05 14:41:36 -0300

    feat(paystub): add paystub api get profiles and links

--- src/app/civil-servant/pages/paystub/paystub.module.ts
+++ src/app/civil-servant/pages/paystub/paystub.module.ts
@@ -6,6 +6,7 @@ import { PaycheckSheetComponentModule } from '../../components/paycheck-sheet';
 import { PaystubProfilesComponentModule } from '../../components/paystub-profiles';
 import { PaystubLinksComponentModule } from '../../components/paystub-links';
 import { PaystubDownloadComponentModule } from '../../components/paystub-download';
+import { PaystubApiService } from '../../providers/paystub.api.service'
 
 @NgModule({
   declarations: [
@@ -19,5 +20,8 @@ import { PaystubDownloadComponentModule } from '../../components/paystub-downloa
     PaystubLinksComponentModule,
     PaystubDownloadComponentModule,
   ],
+  providers: [
+    PaystubApiService,
+  ]
 })
 export class PaystubPageModule { }
--- src/app/civil-servant/pages/paystub/paystub.ts
+++ src/app/civil-servant/pages/paystub/paystub.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IPaystubLink } from './../../interfaces/link';
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, NavController, NavParams, Loading } from 'ionic-angular';
+import { PaystubApiService } from '../../providers/paystub.api.service';
+import { AuthService, AcessoCidadaoClaims, LoadingService } from '@espm/core';
+import { IPaystubProfile } from '../../interfaces';
+import { Observable } from 'rxjs/Observable';
+import { mergeMap, finalize } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
 
 @IonicPage({
   segment: 'contracheque'
@@ -8,9 +15,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'paystub',
   templateUrl: 'paystub.html'
 })
-export class PaystubPage {
+export class PaystubPage implements OnInit {
 
-  activeComponent: 'profile' | 'links' | 'download' = 'download'
-  constructor(public navCtrl: NavController, public navParams: NavParams) { }
+  activeComponent: 'profile' | 'links' | 'download' = 'profile';
+  profiles$: Observable<IPaystubProfile[]> = of([]);
+  links$: Observable<IPaystubLink[]> = of([]);
+  private currentUser: AcessoCidadaoClaims
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private api: PaystubApiService, private auth: AuthService, private loading: LoadingService) { }
+
+  ngOnInit(): void {
+    const loading = this.loading.show('Aguarde');
+    this.auth.refreshAccessTokenIfNeeded()
+      .pipe(mergeMap(() => this.auth.refreshUser()))
+      .subscribe(user => {
+        this.currentUser = user;
+        this.getProfiles(loading);
+      });
+  }
+
+  getProfiles(loading: Loading): void {
+    this.profiles$ = this.api.getProfiles(this.currentUser.cpf).pipe(
+      finalize(() => loading.dismiss())
+    );
+  }
+
+  getLinks(profile: IPaystubProfile): void {
+    const loading = this.loading.show('Aguarde');
+    this.activeComponent = 'links';
+    this.links$ = this.api.getLink(this.currentUser.cpf, profile.codigoPerfil, profile.numeroFuncionario).pipe(
+      finalize(() => loading.dismiss())
+    );
+  }
 
 }
