4ee42371082fd6ce0c0fa5ec27082f8d3c6f1389
David Vilaça <vilacapdavid@gmail.com>
2019-12-05 14:40:53 -0300

    fix(links-status): uppercase scss class

--- src/app/civil-servant/components/links-status/links-status.component.ts
+++ src/app/civil-servant/components/links-status/links-status.component.ts
@@ -6,5 +6,5 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LinksStatusComponent {
-  @Input() status;
+  @Input() status: string;
 }
