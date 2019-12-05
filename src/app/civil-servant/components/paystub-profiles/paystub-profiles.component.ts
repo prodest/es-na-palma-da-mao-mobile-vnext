48a108a22e34e0b47f11948fe701ff9c89b6bf9c
David Vilaça <vilacapdavid@gmail.com>
2019-12-05 14:39:23 -0300

    feat(paystub-profiles): add dynamic profiles input and output

--- src/app/civil-servant/components/paystub-profiles/paystub-profiles.component.ts
+++ src/app/civil-servant/components/paystub-profiles/paystub-profiles.component.ts
@@ -1,24 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { NavController, NavParams, /*AlertController*/ } from 'ionic-angular';
+import { IPaystubProfile } from '../../interfaces/profile';
 
 @Component({
   selector: 'paystub-profiles',
   templateUrl: 'paystub-profiles.component.html'
 })
 export class PaystubProfilesComponent {
+
+  @Input() profiles: IPaystubProfile[] = [];
+  @Output() onSelectProfile: EventEmitter<IPaystubProfile> = new EventEmitter();
+
   constructor(public navCtrl: NavController, public navParams: NavParams, /*private alert: AlertController*/) { }
 
-  profiles = [
-    {
-      perfilacesso: 'Servidor', disable: false
-    },
-    {
-      perfilacesso: 'Pensionista', disable: false
-    },
-    {
-      perfilacesso: 'Pensão Especial', disable: false
-    }
-  ];
 
   // showDetails(profile: any): void {
   //   const alert = this.alert.create({
@@ -30,14 +24,8 @@ export class PaystubProfilesComponent {
   //   alert.present();
   // }
 
-  pushNext(tipe) {
-    // this.service
-    //   .getDebitsTipe(this.vehicle, tipe)
-    //   .subscribe(
-    //     debits =>
-    //       this.navCtrl.push('VehicleDebitsPage', { tipe: tipe, vehicle: this.vehicle, plate: this.vehicle.plate, debits }),
-    //     error => console.log(error)
-    //   );
+  selectProfile(profile: IPaystubProfile) {
+    this.onSelectProfile.emit(profile);
   }
 }
 
