770957967a8ff793a75ec5f63602b9d6bca7edc7
David Vilaça <vilacapdavid@gmail.com>
2019-12-05 14:38:28 -0300

    feat(paystub): add paystub api service profiles and links

--- src/app/civil-servant/providers/paystub.api.service.ts
+++ src/app/civil-servant/providers/paystub.api.service.ts
@@ -1,19 +1,37 @@
-/*import { ApiBaseService, EnvVariables, Environment } from '@espm/core';*/
-import { Injectable, /*Inject*/ } from '@angular/core';
-/*import { HttpClient } from '@angular/common/http';*/
+import { EnvVariables, Environment } from '@espm/core';
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IPaystubProfile, IPaystubLink } from '../interfaces';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
-export class PaystubApiService /*extends ApiBaseService<any>*/ {
-  
-  constructor(/*private http: HttpClient, @Inject(EnvVariables) env: Environment*/) {
-    /*super(`https://api.es.gov.br/siarhes/`);*/
+export class PaystubApiService {
+
+  private readonly api: string;
+  constructor(private http: HttpClient, @Inject(EnvVariables) env: Environment) {
+    this.api = env.api.paystub;
   }
 
-  getProfiles(cpf: string) {}
+  getProfiles(cpf: string): Observable<IPaystubProfile[]> {
+    return this.http.get<IPaystubProfile[]>(this.endpoint('perfis'), { params: { cpf } })
+  }
 
-  getLink(perfil: string) {}
+  getLink(cpf: string, codPerfil: number, numFunc: number): Observable<IPaystubLink[]> {
+    return this.http.get<IPaystubLink[]>(this.endpoint('vinculos'), {
+      params: {
+        cpf,
+        codPerfil: codPerfil.toString(),
+        numFunc: numFunc.toString()
+      }
+    })
+  }
 
-  getLeaf(link: string) {}
+  getLeaf(link: string) { }
 
-  getPaystub(leaf: string) {}
+  getPaystub(leaf: string) { }
+
+  private endpoint(route: number | string): string {
+    const api: string = this.api.charAt(this.api.length - 1) === '/' ? this.api.substr(this.api.length - 1) : this.api;
+    return !route ? api : `${api}/${route}`.trim();
+  }
 }
