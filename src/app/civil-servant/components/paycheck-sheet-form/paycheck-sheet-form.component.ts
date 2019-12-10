433b483b6f82107b19cee6dd0451aceaa938a19f
David Vilaça <vilacapdavid@gmail.com>
2019-12-09 23:43:39 -0300

    feat(paycheck-sheet-form): get and update data by input and output

--- src/app/civil-servant/components/paycheck-sheet-form/paycheck-sheet-form.component.ts
+++ src/app/civil-servant/components/paycheck-sheet-form/paycheck-sheet-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, OnChanges, SimpleChanges, ChangeDetectionStrategy, ChangeDetectorRef, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, ChangeDetectionStrategy, ChangeDetectorRef, Input, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormBase } from '@espm/core';
+import { IPaystubYear, IPaystubMonth } from '../../interfaces';
 
 @Component({
   selector: 'civil-servant-paycheck-sheet-form',
@@ -9,9 +10,11 @@ import { FormBase } from '@espm/core';
 })
 export class PaycheckSheetFormComponent extends FormBase implements OnInit, OnChanges {
 
-  @Input() years: string[] = ['2018', '2019'];
-  @Input() months: string[] = ['Outubro', 'Novembro'];
-  @Input() sheets: number[] = [31, 32];
+  @Input() years: IPaystubYear[] = [];
+  @Output() onSelectYear: EventEmitter<IPaystubYear> = new EventEmitter();
+  @Input() months: IPaystubMonth[] = [];
+  @Output() onSelectMonth: EventEmitter<IPaystubMonth> = new EventEmitter();
+  @Input() sheets: number[] = [];
 
   validationMessages = {
     year: {
@@ -41,13 +44,34 @@ export class PaycheckSheetFormComponent extends FormBase implements OnInit, OnCh
     subTitle: 'Selecione a folha referente ao contracheque que deseja consultar'
   };
 
+  monthsValues = ['', 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
   constructor(formBuilder: FormBuilder, private cdr: ChangeDetectorRef) {
     super(formBuilder);
   }
 
   ngOnInit(): void { }
 
-  ngOnChanges(changes: SimpleChanges): void { }
+  ngOnChanges(changes: SimpleChanges): void {
+    if ('years' in changes) {
+      const years: Array<IPaystubYear> = changes['years'].currentValue || [];
+      this.years = years.sort().reverse()
+    }
+    if ('months' in changes) {
+      const months: Array<IPaystubMonth> = changes['months'].currentValue || [];
+      this.months = months.sort().reverse()
+    }
+  }
+
+  selectYear(year: IPaystubYear): void {
+    this.onSelectYear.emit(year);
+    return this.selectChange();
+  }
+
+  selectMonth(month: IPaystubMonth): void {
+    this.onSelectMonth.emit(month);
+    return this.selectChange();
+  }
 
   selectChange = () => {
     this.cdr.detectChanges();
@@ -55,9 +79,9 @@ export class PaycheckSheetFormComponent extends FormBase implements OnInit, OnCh
 
   protected createFormModel(): FormGroup {
     return this.formBuilder.group({
-      year: ['', [Validators.required]],
-      month: ['', [Validators.required]],
-      sheet: ['', [Validators.required]]
+      year: [null, [Validators.required]],
+      month: [null, [Validators.required]],
+      sheet: [null, [Validators.required]]
     });
   }
 
