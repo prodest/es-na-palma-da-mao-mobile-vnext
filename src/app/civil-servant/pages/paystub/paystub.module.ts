dd42b72e25d66712fbdffcba556dcfe7ddc0418f
David Vilaça <vilacapdavid@gmail.com>
2019-12-09 23:45:47 -0300

    feat(paystub): get paystub data by service

--- src/app/civil-servant/pages/paystub/paystub.module.ts
+++ src/app/civil-servant/pages/paystub/paystub.module.ts
@@ -6,6 +6,7 @@ import { PaycheckSheetComponentModule } from '../../components/paycheck-sheet';
 import { PaystubProfilesComponentModule } from '../../components/paystub-profiles';
 import { PaystubLinksComponentModule } from '../../components/paystub-links';
 import { PaystubDownloadComponentModule } from '../../components/paystub-download';
+import { PaystubService } from '../../providers/paystub.service';
 import { PaystubApiService } from '../../providers/paystub.api.service'
 
 @NgModule({
@@ -21,6 +22,7 @@ import { PaystubApiService } from '../../providers/paystub.api.service'
     PaystubDownloadComponentModule,
   ],
   providers: [
+    PaystubService,
     PaystubApiService,
   ]
 })
--- src/app/civil-servant/pages/paystub/paystub.ts
+++ src/app/civil-servant/pages/paystub/paystub.ts
@@ -1,9 +1,8 @@
-import { IPaystubLink } from './../../interfaces/link';
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams, Loading } from 'ionic-angular';
-import { PaystubApiService } from '../../providers/paystub.api.service';
+import { PaystubService } from '../../providers/paystub.service';
 import { AuthService, AcessoCidadaoClaims, LoadingService } from '@espm/core';
-import { IPaystubProfile } from '../../interfaces';
+import { IPaystubProfile, IPaystubLink, IPaystubYear, IPaystubMonth } from '../../interfaces';
 import { Observable } from 'rxjs/Observable';
 import { mergeMap, finalize } from 'rxjs/operators';
 import { of } from 'rxjs/observable/of';
@@ -20,10 +19,16 @@ export class PaystubPage implements OnInit {
   activeComponent: 'profile' | 'links' | 'download' = 'profile';
   profiles$: Observable<IPaystubProfile[]> = of([]);
   links$: Observable<IPaystubLink[]> = of([]);
-  private currentUser: AcessoCidadaoClaims
+  years$: Observable<IPaystubYear[]> = of([]);
+  months$: Observable<IPaystubMonth[]> = of([]);
+  currentUser: AcessoCidadaoClaims | undefined;
+  currentProfile: IPaystubProfile | undefined;
+  currentLink: IPaystubLink | undefined;
+  currentYear: IPaystubYear | undefined;
+  currentMonth: IPaystubMonth | undefined;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    private api: PaystubApiService, private auth: AuthService, private loading: LoadingService) { }
+    private paystubService: PaystubService, private auth: AuthService, private loading: LoadingService) { }
 
   ngOnInit(): void {
     const loading = this.loading.show('Aguarde');
@@ -36,17 +41,55 @@ export class PaystubPage implements OnInit {
   }
 
   getProfiles(loading: Loading): void {
-    this.profiles$ = this.api.getProfiles(this.currentUser.cpf).pipe(
+    if (!this.currentUser) { return; }
+    this.profiles$ = this.paystubService.getProfiles(this.currentUser.cpf).pipe(
       finalize(() => loading.dismiss())
     );
   }
 
-  getLinks(profile: IPaystubProfile): void {
+  getLinks(profile?: IPaystubProfile): void {
+    if (!profile) { return; }
     const loading = this.loading.show('Aguarde');
     this.activeComponent = 'links';
-    this.links$ = this.api.getLink(this.currentUser.cpf, profile.codigoPerfil, profile.numeroFuncionario).pipe(
+    this.currentProfile = profile;
+    this.links$ = this.paystubService.getLink(this.currentUser.cpf, profile.codigoPerfil, profile.numeroFuncionario).pipe(
       finalize(() => loading.dismiss())
     );
+  }
+
+  getYears(link?: IPaystubLink): void {
+    if (!this.currentProfile || !link) { return; }
+    const loading = this.loading.show('Aguarde');
+    const { numeroFuncionario } = this.currentProfile;
+    const {
+      numeroVinculo,
+      numeroPensionista
+    } = link;
+    this.activeComponent = 'download';
+    this.currentLink = link;
+    this.years$ = this.paystubService.getYears(numeroFuncionario, numeroVinculo, numeroPensionista).pipe(
+      finalize(() => loading.dismiss())
+    );
+  }
+
+  getMonths(year?: IPaystubYear): void {
+    if (!this.currentLink || !year) { return; }
+    const loading = this.loading.show('Aguarde');
+    const { numeroFuncionario } = this.currentProfile;
+    const {
+      numeroVinculo,
+      numeroPensionista
+    } = this.currentLink;
+    this.activeComponent = 'download';
+    this.currentYear = year;
+    this.months$ = this.paystubService.getMonths(numeroFuncionario, numeroVinculo, year, numeroPensionista).pipe(
+      finalize(() => loading.dismiss())
+    );
+  }
+
+  getLeafs(month?: IPaystubMonth): void {
+    if (!this.currentYear || !month) { return; }
+    console.log('month selected = ' + month)
   }
 
 }
