3ec626b9882fedaff3159917359becb11024c260
David Vilaça <vilacapdavid@gmail.com>
2019-12-11 10:32:24 -0300

    fix(paystub): rm unknow import module

--- src/app/civil-servant/pages/paystub/paystub.module.ts
+++ src/app/civil-servant/pages/paystub/paystub.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { PaystubPage } from './paystub';
 import { ModulePageComponentModule } from '@espm/shared';
-import { PaycheckSheetComponentModule } from '../../components/paycheck-sheet';
 import { PaystubProfilesComponentModule } from '../../components/paystub-profiles';
 import { PaystubLinksComponentModule } from '../../components/paystub-links';
 import { PaystubDownloadComponentModule } from '../../components/paystub-download';
@@ -15,7 +14,6 @@ import { PaystubApiService } from '../../providers/paystub.api.service'
   ],
   imports: [
     ModulePageComponentModule,
-    PaycheckSheetComponentModule,
     IonicPageModule.forChild(PaystubPage),
     PaystubProfilesComponentModule,
     PaystubLinksComponentModule,
